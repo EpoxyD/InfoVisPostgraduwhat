2dd09d06aa193422a3e20e724c8e0ab7825ca745
Wouter Van der Schraelen <wvanderschraelen@gmail.com>
2016-03-03 11:17:50 +0100

    Extracted consumption and timestamp

--- dataVisScript.js
+++ dataVisScript.js
@@ -1,14 +1,8 @@
-d3.csv("dataSet.csv", function(data) {
+d3.csv("MOCK_DATA.csv", function(data) {
     data.forEach(function(d) {
         // hier map je de data uit de csv aan het "data" object (de '+' is om aan te geven dat het een getalwaarde is
-        d.id = +d.id;
-        d.objectid = +d.objectid;
-        d.point_lat = +d.point_lat;
-        d.point_lng = +d.point_lng;
-        d.straatnaam = d.straatnaam;
-        d.huisnummer = +d.huisnummer;
-        d.district = d.district;
-        d.postcode = +d.postcode
+        d.timeStamp = d.TS;
+        d.consumption = +d.Cons;
     });
 
 
