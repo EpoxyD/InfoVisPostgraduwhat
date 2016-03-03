ad5ed141350c93b876221524d9572d24279b3343
Wouter Van der Schraelen <wvanderschraelen@gmail.com>
2016-03-03 12:57:13 +0100

    Changed mock_data.csv timestamp and constant collumn names

--- dataVisScript.js
+++ dataVisScript.js
@@ -7,6 +7,15 @@ var width = outerWidth - margin.left - margin.right;
 
 var height = outerHeight - margin.top - margin.bottom;
 
+var days = [
+     { "Day": "Monday", "DayNumber": 0},
+     { "Day": "Tuesday", "DayNumber": 1},
+     { "Day": "Wednesday", "DayNumber": 2},
+     { "Day": "Thursday", "DayNumber": 3},
+     { "Day": "Friday", "DayNumber": 4},
+     { "Day": "Saturday", "DayNumber": 5},
+     { "Day": "Sunday", "DayNumber": 6}];
+
 var svg = d3.select("body").append("svg")
     .attr("width",outerWidth)
     .attr("height",outerHeight  )
@@ -17,9 +26,9 @@ var svg = d3.select("body").append("svg")
 d3.csv("MOCK_DATA.csv", function(data) {
     data.forEach(function(d) {
         // hier map je de data uit de csv aan het "data" object (de '+' is om aan te geven dat het een getalwaarde is
-        d.timeStamp = d.TS;
-        d.consumption = +d.Cons;
-        d.dateParts = d.TS.match(/(\d+)-(\d+)-(\d+) (\d+):(\d+):(\d+)/);
+        d.timeStamp = d.Timestamp;
+        d.consumption = +d.Consumption;
+        d.dateParts = d.Timestamp.match(/(\d+)-(\d+)-(\d+) (\d+):(\d+):(\d+)/);
         d.date = new Date(d.dateParts[1], d.dateParts[2], d.dateParts[3], d.dateParts[4], d.dateParts[5], d.dateParts[6], d.dateParts[6]);
         d.hour = d.date.getHours();
         d.dayNumber = d.date.getDay();
@@ -51,7 +60,7 @@ d3.csv("MOCK_DATA.csv", function(data) {
             return calculateXCoordinate(d.dayNumber, d.hour);
         })
         .attr("y", function(d) {
-            return height/2;
+            return 0;
         })
         .attr("width", blockWidth)
         .attr("height", 20)
